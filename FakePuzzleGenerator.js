4383ffe0693e2f73f1b14bb44dbcb43050e33975
andrasfelfoldi <andras.felfoldi@gmail.com>
2019-01-29 06:56:24 +0100

    exposed methods to get puzzles

--- FakePuzzleGenerator.js
+++ FakePuzzleGenerator.js
@@ -3,25 +3,35 @@ utils = require("./Utils");
 transformer = require('./TableTransformer');
 randomGenerator = require('./Random');
 
-puzzle = basePuzzles.getRandomMedium();
+_transformBaseSudokuPuzzle = (puzzle) => {
+    puzzle = transformer.shuffleColumns(puzzle);
+    puzzle = transformer.shuffleRows(puzzle);
+    puzzle = transformer.rotateTable(puzzle, randomGenerator.getRandomIntBetween(0, 4));
+    if(randomGenerator.getRandomIntBetween(0, 2)){
+        puzzle = transformer.transponseTable(puzzle);
+    }
+    if(randomGenerator.getRandomIntBetween(0, 2)){
+        puzzle = transformer.mirrorTable(puzzle);
+    }
+    puzzle = transformer.randomizeTableNumbers(puzzle);
 
-utils.printTable(puzzle);
-
-puzzle = transformer.shuffleColumns(puzzle);
-puzzle = transformer.shuffleRows(puzzle);
-puzzle = transformer.rotateTable(puzzle, randomGenerator.getRandomIntBetween(0, 4));
-if(randomGenerator.getRandomIntBetween(0, 2)){
-    puzzle = transformer.transponseTable(puzzle);
+    return puzzle;
 }
-if(randomGenerator.getRandomIntBetween(0, 2)){
-    puzzle = transformer.mirrorTable(puzzle);
+
+module.exports.getEasySudoku = () => {
+    let puzzle = basePuzzles.getRandomEasy();
+    puzzle = _transformBaseSudokuPuzzle(puzzle);
+    return puzzle;
 }
-puzzle = transformer.randomizeTableNumbers(puzzle);
 
-console.log('\n==================================================');
-console.log('||||||||||||||||||||||||||||||||||||||||||||||||||');
-console.log('==================================================\n');
+module.exports.getMediumSudoku = () => {
+    let puzzle = basePuzzles.getRandomMedium();
+    puzzle = _transformBaseSudokuPuzzle(puzzle);
+    return puzzle;
+}
 
-console.log(utils.tableToString(puzzle));
-
-// console.log(utils.tableToString(puzzle));
+module.exports.getHardSudoku = () => {
+    let puzzle = basePuzzles.getRandomHard();
+    puzzle = _transformBaseSudokuPuzzle(puzzle);
+    return puzzle;
+}
